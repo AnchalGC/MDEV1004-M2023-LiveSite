8af80b0e6f7fcc9a2c9b9e97eff72d049afe1b6b
AnchalGC <ranaanchal2012@gmail.com>
2023-06-15 23:16:05 -0400

    corrected json

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let localURI = 'mongodb://127.0.0.1:27017/movies';
 let remoteURI = 'mongodb+srv://AnchalRana:EUUBPaJbxpDsSPrJ@moviecluster.2nhi2sn.mongodb.net/movies';
+let secret = 'SomeSecret';
 exports.default = {
     localURI: localURI,
-    remoteURI: remoteURI
+    remoteURI: remoteURI,
+    secret: secret
 };
 //# sourceMappingURL=db.js.map
